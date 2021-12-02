8f6ab789b12a30332ad66a6a6fa7e606f4f60ae9
swakumar <swakumar@SWAKUMAR-IN.in.oracle.com>
2021-12-02 17:58:07 +0530

    adding test class

---
+++ src/test/java/com/swagat/TestClass.java
@@ -0,0 +1,12 @@
+package com.swagat;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test maven-docker success");
+		System.out.println("tetetet");
+
+	}
+
+}
